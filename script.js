a2b4523c55fd3ca4240312f1744d0a7618f25e22
Tharun Kumar <53345192+jig-s4w@users.noreply.github.com>
2023-01-28 00:07:56 +0530

    js for numbers to display

--- script.js
+++ script.js
@@ -11,6 +11,8 @@
 //     console.log(e.target.textContent)
 // }
 
+const display = document.querySelector(".display-span")
+
 if (document.body.addEventListener){
     document.body.addEventListener('click',yourHandler,false);
 }
@@ -18,12 +20,23 @@ else{
     document.body.attachEvent('onclick',yourHandler);//for IE
 }
 
+
+
 function yourHandler(e){
     e = e || window.event;
     var target = e.target || e.srcElement;
     if (target.className.match("numbers"))
     {
         console.log(e.target.textContent)
+        display.textContent = display.textContent + e.target.textContent
         //an element with the keyword Class was clicked
     }
+    if (target.className.match("operations"))
+    {
+        console.log(e.target.textContent)
+    }
+}
+
+function operation(e){
+    
 }
